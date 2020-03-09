8aca0787501a1cad7a11a06c870d32fea4df1d6b
Hugh Ormond <hghrmnd@gmail.com>
2020-03-09 17:21:11 +0000

    Changed login cookies

--- frontend/common.js
+++ frontend/common.js
@@ -1,9 +1,14 @@
-var getFromCookie = function(name) {
-    var cookie = decodeURIComponent(document.cookie);
+var getFromCookie = function(name, cookie) {
     var cookies = cookie.split(';');
 
     for (var i = 0; i < cookies.length; i++) {
+        var str = cookies[i];
 
+        console.log("i: " + i + " str: " + str);
+        console.log("Index: " + str.indexOf(name));
+        if (str.indexOf(name) === 0) {
+            return str.substring(name.length, str.length);
+        }
     }
 
     console.log("Could not load cookie: " + name);
