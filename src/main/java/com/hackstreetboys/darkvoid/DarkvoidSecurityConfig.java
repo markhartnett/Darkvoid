597f8d88e51845100a1e0326e9ef97509f974119
Hugh Ormond <hghrmnd@gmail.com>
2020-05-15 06:33:00 +0100

    Begin session rework

--- src/main/java/com/hackstreetboys/darkvoid/DarkvoidSecurityConfig.java
+++ src/main/java/com/hackstreetboys/darkvoid/DarkvoidSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
@@ -20,6 +21,9 @@ public class DarkvoidSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .csrf()
                 .disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
     }
 
     @Bean
